0f5a39e81787eef66a095c00cad003a30672527e
James Jilhaney <notail2111@gmail.com>
2022-04-26 19:19:52 +0800

    Firebase connection working

---
+++ src/script.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+// import { getAuth } from "firebase/auth";
+
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDJPYO9Qo6ORoLV4E9ThEaflElXYQqIpcQ",
+    authDomain: "bilin-346803.firebaseapp.com",
+    projectId: "bilin-346803",
+    storageBucket: "bilin-346803.appspot.com",
+    messagingSenderId: "120490219952",
+    appId: "1:120490219952:web:97099f3306886b175e1e75",
+    measurementId: "G-90MCYRQP2X"
+});
+
+const db = getFirestore(firebaseApp);
+
+const colRef = collection(db, 'events')
+
+getDocs(colRef).then((snapshot) => {
+    console.log(snapshot.docs)
+})
