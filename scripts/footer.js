a00686975bd5b7da1caaa64ecf8334c4ded106eb
zanne <tan.zanne99@gmail.com>
2022-05-07 22:04:07 +0800

    Added js for reusable components

--- scripts/footer.js
+++
@@ -1,8 +0,0 @@
-fetch('/src/footer.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_footer");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
