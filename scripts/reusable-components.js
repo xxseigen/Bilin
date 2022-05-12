6286329a1022aee91f9965668b0e9b10ca81bc7f
James Jilhaney <20190016830@my.xu.edu.ph>
2022-05-12 19:07:13 +0800

    Changed format of directory for firebase hosting

--- scripts/reusable-components.js
+++ scripts/reusable-components.js
@@ -1,21 +1,21 @@
 //Nav bar
-fetch('/src/nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+fetch('/public/src/nav.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_navbar");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
 
 
 
 //Footer
-fetch('/src/footer.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_footer");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+fetch('/public/src/footer.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#replace_with_footer");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
